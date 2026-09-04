ab893bada0cef5989de32ddf166d31b29a3d22b1
chars29million <rcharleskianne@gmail.com>
2026-09-05 00:01:32 +0800

    Add user and message models, implement MongoDB connection, and set up health check endpoint

---
+++ backend/src/lib/db.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+export async function connectDB() {
+  try {
+    const mongoURI = process.env.MONGO_URI;
+    if (!mongoURI) {
+      throw new Error("MONGO_URI is required");
+    }
+
+    const conn = await mongoose.connect(mongoURI);
+
+    console.log("MongoDB is connected", conn.connection.host);
+  } catch (error) {
+    console.error("MongoDB connection error", error.message);
+    process.exit(1);
+    //1 means failed 0  means success
+  }
+}
