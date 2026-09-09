12346c3eb203e9c714425b78794c15c524f82ad1
chars29million <rcharleskianne@gmail.com>
2026-09-09 21:59:49 +0800

    Fix express.json middleware usage in index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT;
 const FRONTEND_URL = process.env.FRONTEND_URL;
 const publicDir = path.join(path.resolve(), "public");
 
-app.use(express.json);
+app.use(express.json());
 app.use(cors({ origin: FRONTEND_URL, credentials: true }));
 app.use(clerkMiddleware());
 
