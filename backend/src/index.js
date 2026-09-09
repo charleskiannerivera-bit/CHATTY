c298acdb174674dc8c4e8663f1f8bedaa2c4f267
chars29million <rcharleskianne@gmail.com>
2026-09-09 22:28:13 +0800

    Fix import statement for cron job in index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,7 +7,7 @@ import { clerkMiddleware } from "@clerk/express";
 import dns from "dns";
 import User from "./models/User.js";
 import { connectDB } from "./lib/db.js";
-import { job } from "./lib/cron.js";
+import job from "./lib/cron.js";
 
 dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
