349c8fbb53bad5ec23e8a0fa2dfe4ed7fcb8dffd
chars29million <rcharleskianne@gmail.com>
2026-09-16 23:18:12 +0800

    feat: implement PageLoader component for improved loading experience and update App component to use it

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,35 +4,38 @@ import { Navigate, BrowserRouter, Routes, Route } from "react-router-dom";
 import ChatPage from "./pages/ChatPage.jsx";
 import AuthPage from "./pages/AuthPage.jsx";
 import { useAuth } from "@clerk/react";
+import PageLoader from "./components/PageLoader.jsx";
 
 function App() {
   const { isSignedIn, isLoaded } = useAuth();
-
   if (!isLoaded) {
-    return <div>Loading...</div>;
+    return <PageLoader />;
   }
-
-  // Set this to true when ChatPage is ready
-  const chatPageReady = false;
-
   return (
     <BrowserRouter>
+      {" "}
       <ThemeProvider>
+        {" "}
         <WallpaperProvider>
+          {" "}
           <Routes>
+            {" "}
             <Route
               path="/"
               element={
-                chatPageReady && isSignedIn ? <ChatPage /> : <AuthPage />
+                isSignedIn ? <ChatPage /> : <Navigate to={"/auth"} replace />
               }
-            />
-
-            <Route path="/auth" element={<AuthPage />} />
-          </Routes>
-        </WallpaperProvider>
-      </ThemeProvider>
+            />{" "}
+            <Route
+              path="/auth"
+              element={
+                !isSignedIn ? <AuthPage /> : <Navigate to={"/"} replace />
+              }
+            />{" "}
+          </Routes>{" "}
+        </WallpaperProvider>{" "}
+      </ThemeProvider>{" "}
     </BrowserRouter>
   );
 }
-
 export default App;
