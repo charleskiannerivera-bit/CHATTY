bbb661b8b2f5dfdc39b13c8d6f112018a0508fa8
chars29million <rcharleskianne@gmail.com>
2026-09-24 11:52:37 +0800

    feat: add PWA support with vite-plugin-pwa and update package.json

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,12 +2,32 @@ import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import babel from "@rolldown/plugin-babel";
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] }),
     tailwindcss(),
+
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Chatty",
+        short_name: "Chatty",
+        description: "A real-time messaging application",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+        start_url: "/",
+        icons: [
+          {
+            src: "/auth.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
   ],
 });
